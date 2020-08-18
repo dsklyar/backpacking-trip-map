98147ed27f03dfdb2df925a1eca536295d00355e
Daniel <danielksklyar@gmail.com>
2020-08-18 16:29:52 -0700

    -Added Legend componets for better EX

---
+++ src/components/legend/index.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { createUseStyles } from "react-jss";
+import { styles } from "./styles";
+
+const useStyles = createUseStyles(styles);
+
+enum KeyEnum {
+	W = "KeyW",
+	A = "KeyA",
+	S = "KeyS",
+	D = "KeyD",
+	E = "KeyE",
+	Q = "KeyQ",
+}
+
+export const Legend: React.FC = () => {
+	const classes = useStyles();
+	return (
+		<div className={classes.container}>
+			<div>
+				<span>WASD to move</span>
+				<div className={classes.wasdContainer}>
+					<Key keyCode={KeyEnum.W} letter={"W"} />
+					<div className={classes.bottom}>
+						<Key keyCode={KeyEnum.A} letter={"A"} />
+						<Key keyCode={KeyEnum.S} letter={"S"} />
+						<Key keyCode={KeyEnum.D} letter={"D"} />
+					</div>
+				</div>
+			</div>
+			<div>
+				<span>QE to zoom</span>
+				<div className={classes.qeContainer}>
+					<Key keyCode={KeyEnum.Q} letter={"Q"} />
+					<Key keyCode={KeyEnum.E} letter={"E"} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+interface IProps {
+	letter: string;
+	keyCode: KeyEnum;
+}
+
+const Key: React.FC<IProps> = ({ letter, keyCode }) => {
+	const classes = useStyles();
+	const [keyToggled, setKeyToggle] = React.useState<boolean>(false);
+
+	React.useEffect(() => {
+		const onKeyDown = (e: KeyboardEvent) => {
+			if (e.code === keyCode && !keyToggled) {
+				setKeyToggle(true);
+			}
+		};
+		const onKeyUp = (e: KeyboardEvent) => {
+			if (e.code === keyCode && keyToggled) {
+				setKeyToggle(false);
+			}
+		};
+
+		document.addEventListener("keydown", onKeyDown);
+		document.addEventListener("keyup", onKeyUp);
+		return () => {
+			document.removeEventListener("keydown", onKeyDown);
+			document.removeEventListener("keyup", onKeyUp);
+		};
+	}, [keyCode, keyToggled]);
+
+	const className = keyToggled ? classes.active : classes.key;
+
+	return (
+		<div className={className}>
+			<span>{letter}</span>
+		</div>
+	);
+};
---
+++ src/components/legend/styles.ts
@@ -0,0 +1,58 @@
+export const styles = {
+	container: {
+		display: "flex",
+		alignItems: "flex-end",
+		"& > div": {
+			display: "flex",
+			flexDirection: "column",
+			"&> span": {
+				fontFamily: "Roboto",
+				fontSize: 17,
+				color: "#b4b4b4",
+				margin: "10px auto",
+				alignSelf: "center",
+			},
+		},
+	},
+	qeContainer: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+	},
+	wasdContainer: {
+		width: "fit-content",
+		"& > $key, > $active": {
+			margin: "auto",
+			marginTop: "2.5px",
+			marginBottom: "2.5px",
+		},
+	},
+	bottom: {
+		display: "flex",
+	},
+	key: {
+		margin: "2.5px",
+		fontFamily: "Roboto",
+		fontSize: 22,
+		paddingTop: 12,
+		paddingLeft: 10,
+		boxSizing: "border-box",
+		border: "1.5px solid #c4c4c4",
+		borderRadius: 15,
+		color: "#c4c4c4",
+		width: 50,
+		height: 50,
+		display: "flex",
+		// justifyContent: "center",
+		alignItems: "center",
+		boxShadow: `0px 2px 1px -1px rgba(0,0,0,0.2),
+								0px 1px 1px 0px rgba(0,0,0,0.14),
+								0px 1px 3px 0px rgba(0,0,0,0.12)`,
+	},
+	active: {
+		extend: "key",
+		backgroundColor: "#c4c4c4",
+		color: "#fff",
+		boxShadow: "none",
+	},
+};
